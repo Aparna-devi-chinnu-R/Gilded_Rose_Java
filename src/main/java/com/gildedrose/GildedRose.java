e31ede5bb13558a318e896971c2797fd0389a1c4
Aparna-devi-chinnu-R <aparnadevi.rajavel@thoughtworks.com>
2021-11-30 21:17:11 +0530

    [Aparna] Refactor . Shifted upgrading logic to item class.

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -11,51 +11,23 @@ class GildedRose {
         for (Item item : items) {
             if (item.name.equals("Aged Brie")) {
 
-                updateAgedBrie(item);
+                Item.updateAgedBrie(item);
 
             } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
 
-                upgradeBackStage(item);
+                Item.upgradeBackStage(item);
 
             } else if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
 
-                upgradeSulfuras(item);
+                Item.upgradeSulfuras(item);
 
             } else {
 
-                upgradeOther(item);
+                Item.upgradeOther(item);
 
             }
 
         }
     }
 
-    private void upgradeOther(Item item) {
-        if (item.quality > 0) item.quality--;
-        if (item.sellIn < 0 && item.quality > 0) item.quality--;
-
-    }
-
-    private void upgradeSulfuras(Item item) {
-        //does nothing
-    }
-
-    private void upgradeBackStage(Item item) {
-        if (item.quality < 50) {
-            item.quality++;
-
-            if (item.sellIn < 11) item.quality++;
-            if (item.sellIn < 6) item.quality++;
-
-            if (item.sellIn <= 0) item.quality = 0;
-            item.sellIn--;
-        }
-    }
-
-    private void updateAgedBrie(Item item) {
-        if (item.quality < 50) {
-            item.sellIn--;
-            item.quality++;
-        }
-    }
 }
--- src/main/java/com/gildedrose/Item.java
+++ src/main/java/com/gildedrose/Item.java
@@ -14,6 +14,35 @@ public class Item {
         this.quality = quality;
     }
 
+    static void upgradeOther(Item item) {
+        if (item.quality > 0) item.quality--;
+        if (item.sellIn < 0 && item.quality > 0) item.quality--;
+
+    }
+
+    static void upgradeSulfuras(Item item) {
+        //does nothing
+    }
+
+    static void upgradeBackStage(Item item) {
+        if (item.quality < 50) {
+            item.quality++;
+
+            if (item.sellIn < 11) item.quality++;
+            if (item.sellIn < 6) item.quality++;
+
+            if (item.sellIn <= 0) item.quality = 0;
+            item.sellIn--;
+        }
+    }
+
+    static void updateAgedBrie(Item item) {
+        if (item.quality < 50) {
+            item.sellIn--;
+            item.quality++;
+        }
+    }
+
    @Override
    public String toString() {
         return this.name + ", " + this.sellIn + ", " + this.quality;
